8f7a6caff81a8b4d18ac31c4f666d78984f09617
CousinJoey <cousinjoeyllc@gmail.com>
2022-11-25 16:04:29 -0500

    added password check functionality for ensuring matching passwords

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function placeholder() {
-    return "this function is a placeholder"
+
+var check = function() {
+    if (document.getElementById("password").value == document.getElementById("confirm_password").value) {
+        document.getElementById("label_confirm").style.color = "#ff3fe7"
+        document.getElementById("label_pass").style.color = "#ff3fe7"
+    } else {
+        document.getElementById("label_confirm").style.color = "red"
+        document.getElementById("label_pass").style.color = "red"
+    }
+}
+
+const sumbitBtn = document.querySelector(".create_acc")
+
+sumbitBtn.addEventListener("click", passCheck);
+
+function passCheck() {
+    if (document.getElementById("password").value !=
+    document.getElementById("confirm_password").value) {
+        alert("Password Doesn't Match")
+    }
 }
